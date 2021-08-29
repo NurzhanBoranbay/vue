489409c4e58944e83ac010eecd5450059a90e74d
NurzhanBoranbay <boranbaynurzhan3097@gmail.com>
2021-08-29 11:43:25 +0600

    commit exercise-1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <my-components />
+</template>
+
+<script>
+import MyComponents from './components/MyComponents.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponents
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyComponents.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="root">
+      <p>Выберите кол-во продуктов:</p>
+      <button @click="removeNum">-</button>
+      <input type="number" :value="num" :min="minNum" :max="maxNum" name="number" id="">
+      <button @click="addNum">+</button>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'MyComponents',
+   data() {
+      return {
+         minNum: 1,
+         maxNum: 10,
+         num: 1
+      }
+   },
+
+   methods: {
+      addNum() {
+         this.num++;
+         if (this.num > 10) {
+            this.num = 10
+         }
+      },
+      removeNum() {
+         this.num--;
+         if (this.num < 1) {
+            this.num = 1
+         }
+      }
+   }
+}
+</script>
